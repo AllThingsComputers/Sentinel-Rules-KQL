0530dd02c69119f4649ade484e2e3f1d518a76da
All Things Computers <117267670+AllThingsComputers@users.noreply.github.com>
2026-03-29 21:01:32 +0100

    Update format-kql.js

--- scripts/format-kql.js
+++ scripts/format-kql.js
@@ -1,5 +1,5 @@
-import fs from "fs/promises";
-import path from "path";
+const fs = require("fs").promises;
+const path = require("path");
 
 // Configuration: number of spaces per indent level
 const INDENT_SIZE = 2;
